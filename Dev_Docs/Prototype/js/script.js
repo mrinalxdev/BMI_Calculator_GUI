1913f6eb2b304f0f69e1af2ff0d54d27af7bf1fa
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-11-10 00:55:14 +0530

    This is all editing in the Initial Stage

--- Dev_Docs/Prototype/js/script.js
+++ Dev_Docs/Prototype/js/script.js
@@ -13,3 +13,19 @@ document.addEventListener('click', e => {
     dropdown.classList.remove('active')
   })
 })
+
+
+
+
+const slider = docement.querySelector('.slider'); 
+
+const leftArrow = document.querySelector('.left'); 
+const rightArrow = document.querySelector('.right'); 
+
+var sectionIndex = 0;
+
+rightArrow.addEventListener('click', function () {
+  sectionIndex = sectionIndex + 1
+  slider.style.transform = 'translate('+ sectionIndex * -25 +'%)';
+
+})
