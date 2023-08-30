2ebc23b5f63c995c220b0c2810aab997a5574602
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-31 04:55:10 +0530

    Added New Data

--- bmi.py
+++ bmi.py
@@ -6,7 +6,7 @@ class App(ctk.CTk):
         # Window Setup 
         super().__init__(fg_color = GREEN)
         self.title('BMI Calculator')
-        self.geometry('400x400')
+        self.geometry('600x600')
         self.resizable(False, False)
 
         # Layout Setup 
@@ -16,6 +16,8 @@ class App(ctk.CTk):
         # Widget Setup 
         ResultText(self)
         WeightInput(self)
+        HeightInput(self)
+        UnitSwitcher(self)
 
         self.mainloop()
 
@@ -38,11 +40,50 @@ class WeightInput(ctk.CTkFrame):
         self.columnconfigure(3, weight = 1, uniform= 'b')
         self.columnconfigure(4, weight = 2, uniform= 'b')
 
-        # Buttons Setup
+        # Text Setup
         font = ctk.CTkFont(family = FONT, size = INPUT_FONT_SIZE)
         label = ctk.CTkLabel(self, text = '70kg', text_color= BLACK, font = font)
         label.grid(row = 0, column = 2)
 
+        # Buttons Setup 
+        minus_button = ctk.CTkButton(self, text='-', font = font, text_color = BLACK, fg_color= LIGHT_GRAY, hover_color = GRAY, corner_radius= BUTTON_CORNER_RADIUS)
+        minus_button.grid(row = 0, column = 0, sticky = 'ns', padx = 8, pady= 8)
+        
+        plus_button = ctk.CTkButton(self, text='+', font = font, text_color = BLACK, fg_color= LIGHT_GRAY, hover_color = GRAY, corner_radius= BUTTON_CORNER_RADIUS)
+        plus_button.grid(row = 0, column = 4, sticky = 'ns', padx = 8, pady= 8)
+
+        small_plus_button = ctk.CTkButton(self, text='+', font = font, text_color = BLACK, fg_color= LIGHT_GRAY, hover_color = GRAY, corner_radius= BUTTON_CORNER_RADIUS)
+        small_plus_button.grid(row = 0, column = 3, padx = 4, pady= 4)
+        
+        small_minus_button = ctk.CTkButton(self, text='-', font = font, text_color = BLACK, fg_color= LIGHT_GRAY, hover_color = GRAY, corner_radius= BUTTON_CORNER_RADIUS)
+        small_minus_button.grid(row = 0, column = 1, padx = 4, pady= 4)
+
+class HeightInput(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master= parent, fg_color = WHITE)
+        self.grid(row = 3, column = 0, sticky = 'nsew', padx = 10, pady = 10)
+
+        # Widgets Setup 
+        slider = ctk.CTkSlider( master = self, 
+                               button_color= GREEN, 
+                               button_hover_color= GRAY, 
+                               progress_color=GREEN, 
+                               fg_color = LIGHT_GRAY)
+        slider.pack(side= 'left', fill = 'x', expand = True, padx = 10,pady =10 )
+        output_text = ctk.CTkLabel(self, text='1.80m', text_color= BLACK, 
+                                   font = ctk.CTkFont(family = FONT, 
+                                                      size = INPUT_FONT_SIZE))
+        output_text.pack(side= 'left', padx=10, pady=10)
+
+class UnitSwitcher(ctk.CTkLabel):
+    def __init__ (self, parent):
+        super().__init__(master = parent, 
+                         text='metric', 
+                         text_color= DARK_GREEN, 
+                         font= ctk.CTkFont(family = FONT, 
+                                           size= SWITCH_FONT_SIZE, 
+                                           weight= 'bold'))
+        self.place(relx = 0.98, rely = 0.01, anchor = 'ne')
 
 if __name__ == '__main__':
     App()
